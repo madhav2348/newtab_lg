ae670daca9865f5b7efb0543689ba9fcf0422258
Madhav Majumdar <161720210+madhav2348@users.noreply.github.com>
2025-03-31 13:31:11 +0530

    add fonts manually

---
+++ lib/fonts.dart
@@ -0,0 +1,243 @@
+const allFonts = [
+  'Cabin',
+  'Lato',
+  'Montserrar',
+  'Mulish',
+  'Nunito',
+  'Open_Sans',
+  'Poppins',
+  'Raleway',
+  'Roboto_Mono',
+  'Winky_Sans',
+];
+
+
+
+// "\assets\fonts\Roboto_Mono"
+// "\assets\fonts\Winky_Sans"
+// "\assets\fonts\Cabin"
+// "\assets\fonts\Lato"
+// "\assets\fonts\Montserrat"
+// "\assets\fonts\Mulish"
+// "\assets\fonts\Nunito"
+// "\assets\fonts\Open_Sans"
+// "\assets\fonts\Poppins"
+// "\assets\fonts\Raleway"
+
+
+
+// "assets\fonts\Cabin\static\Cabin_Condensed-MediumItalic.ttf"
+// "assets\fonts\Cabin\static\Cabin_Condensed-Regular.ttf"
+// "assets\fonts\Cabin\static\Cabin_Condensed-SemiBold.ttf"
+// "assets\fonts\Cabin\static\Cabin_Condensed-SemiBoldItalic.ttf"
+// "assets\fonts\Cabin\static\Cabin_SemiCondensed-Bold.ttf"
+// "assets\fonts\Cabin\static\Cabin_SemiCondensed-BoldItalic.ttf"
+// "assets\fonts\Cabin\static\Cabin_SemiCondensed-Italic.ttf"
+// "assets\fonts\Cabin\static\Cabin_SemiCondensed-Medium.ttf"
+// "assets\fonts\Cabin\static\Cabin_SemiCondensed-MediumItalic.ttf"
+// "assets\fonts\Cabin\static\Cabin_SemiCondensed-Regular.ttf"
+// "assets\fonts\Cabin\static\Cabin_SemiCondensed-SemiBold.ttf"
+// "assets\fonts\Cabin\static\Cabin_SemiCondensed-SemiBoldItalic.ttf"
+// "assets\fonts\Cabin\static\Cabin-Bold.ttf"
+// "assets\fonts\Cabin\static\Cabin-BoldItalic.ttf"
+// "assets\fonts\Cabin\static\Cabin-Italic.ttf"
+// "assets\fonts\Cabin\static\Cabin-Medium.ttf"
+// "assets\fonts\Cabin\static\Cabin-MediumItalic.ttf"
+// "assets\fonts\Cabin\static\Cabin-Regular.ttf"
+// "assets\fonts\Cabin\static\Cabin-SemiBold.ttf"
+// "assets\fonts\Cabin\static\Cabin-SemiBoldItalic.ttf"
+// "assets\fonts\Cabin\static\Cabin_Condensed-Bold.ttf"
+// "assets\fonts\Cabin\static\Cabin_Condensed-BoldItalic.ttf"
+// "assets\fonts\Cabin\static\Cabin_Condensed-Italic.ttf"
+// "assets\fonts\Cabin\static\Cabin_Condensed-Medium.ttf"
+
+
+
+// "assets\fonts\Lato\Lato-ThinItalic.ttf"
+// "assets\fonts\Lato\OFL.txt"
+// "assets\fonts\Lato\Lato-Black.ttf"
+// "assets\fonts\Lato\Lato-BlackItalic.ttf"
+// "assets\fonts\Lato\Lato-Bold.ttf"
+// "assets\fonts\Lato\Lato-BoldItalic.ttf"
+// "assets\fonts\Lato\Lato-Italic.ttf"
+// "assets\fonts\Lato\Lato-Light.ttf"
+// "assets\fonts\Lato\Lato-LightItalic.ttf"
+// "assets\fonts\Lato\Lato-Regular.ttf"
+// "assets\fonts\Lato\Lato-Thin.ttf"
+
+
+
+// "assets\fonts\Montserrat\static\Montserrat-ThinItalic.ttf"
+// "assets\fonts\Montserrat\static\Montserrat-Black.ttf"
+// "assets\fonts\Montserrat\static\Montserrat-BlackItalic.ttf"
+// "assets\fonts\Montserrat\static\Montserrat-Bold.ttf"
+// "assets\fonts\Montserrat\static\Montserrat-BoldItalic.ttf"
+// "assets\fonts\Montserrat\static\Montserrat-ExtraBold.ttf"
+// "assets\fonts\Montserrat\static\Montserrat-ExtraBoldItalic.ttf"
+// "assets\fonts\Montserrat\static\Montserrat-ExtraLight.ttf"
+// "assets\fonts\Montserrat\static\Montserrat-ExtraLightItalic.ttf"
+// "assets\fonts\Montserrat\static\Montserrat-Italic.ttf"
+// "assets\fonts\Montserrat\static\Montserrat-Light.ttf"
+// "assets\fonts\Montserrat\static\Montserrat-LightItalic.ttf"
+// "assets\fonts\Montserrat\static\Montserrat-Medium.ttf"
+// "assets\fonts\Montserrat\static\Montserrat-MediumItalic.ttf"
+// "assets\fonts\Montserrat\static\Montserrat-Regular.ttf"
+// "assets\fonts\Montserrat\static\Montserrat-SemiBold.ttf"
+// "assets\fonts\Montserrat\static\Montserrat-SemiBoldItalic.ttf"
+// "assets\fonts\Montserrat\static\Montserrat-Thin.ttf"
+
+
+
+// "assets\fonts\Mulish\static\Mulish-BoldItalic.ttf"
+// "assets\fonts\Mulish\static\Mulish-ExtraBold.ttf"
+// "assets\fonts\Mulish\static\Mulish-ExtraBoldItalic.ttf"
+// "assets\fonts\Mulish\static\Mulish-ExtraLight.ttf"
+// "assets\fonts\Mulish\static\Mulish-ExtraLightItalic.ttf"
+// "assets\fonts\Mulish\static\Mulish-Italic.ttf"
+// "assets\fonts\Mulish\static\Mulish-Light.ttf"
+// "assets\fonts\Mulish\static\Mulish-LightItalic.ttf"
+// "assets\fonts\Mulish\static\Mulish-Medium.ttf"
+// "assets\fonts\Mulish\static\Mulish-MediumItalic.ttf"
+// "assets\fonts\Mulish\static\Mulish-Regular.ttf"
+// "assets\fonts\Mulish\static\Mulish-SemiBold.ttf"
+// "assets\fonts\Mulish\static\Mulish-SemiBoldItalic.ttf"
+// "assets\fonts\Mulish\static\Mulish-Black.ttf"
+// "assets\fonts\Mulish\static\Mulish-BlackItalic.ttf"
+// "assets\fonts\Mulish\static\Mulish-Bold.ttf"
+
+
+
+
+// "assets\fonts\Nunito\static\Nunito-ExtraBoldItalic.ttf"
+// "assets\fonts\Nunito\static\Nunito-ExtraLight.ttf"
+// "assets\fonts\Nunito\static\Nunito-ExtraLightItalic.ttf"
+// "assets\fonts\Nunito\static\Nunito-Italic.ttf"
+// "assets\fonts\Nunito\static\Nunito-Light.ttf"
+// "assets\fonts\Nunito\static\Nunito-LightItalic.ttf"
+// "assets\fonts\Nunito\static\Nunito-Medium.ttf"
+// "assets\fonts\Nunito\static\Nunito-MediumItalic.ttf"
+// "assets\fonts\Nunito\static\Nunito-Regular.ttf"
+// "assets\fonts\Nunito\static\Nunito-SemiBold.ttf"
+// "assets\fonts\Nunito\static\Nunito-SemiBoldItalic.ttf"
+// "assets\fonts\Nunito\static\Nunito-Black.ttf"
+// "assets\fonts\Nunito\static\Nunito-BlackItalic.ttf"
+// "assets\fonts\Nunito\static\Nunito-Bold.ttf"
+// "assets\fonts\Nunito\static\Nunito-BoldItalic.ttf"
+// "assets\fonts\Nunito\static\Nunito-ExtraBold.ttf"
+
+
+// "assets\fonts\Open_Sans\static\OpenSans_Condensed-Italic.ttf"
+// "assets\fonts\Open_Sans\static\OpenSans_Condensed-Light.ttf"
+// "assets\fonts\Open_Sans\static\OpenSans_Condensed-LightItalic.ttf"
+// "assets\fonts\Open_Sans\static\OpenSans_Condensed-Medium.ttf"
+// "assets\fonts\Open_Sans\static\OpenSans_Condensed-MediumItalic.ttf"
+// "assets\fonts\Open_Sans\static\OpenSans_Condensed-Regular.ttf"
+// "assets\fonts\Open_Sans\static\OpenSans_Condensed-SemiBold.ttf"
+// "assets\fonts\Open_Sans\static\OpenSans_Condensed-SemiBoldItalic.ttf"
+// "assets\fonts\Open_Sans\static\OpenSans_SemiCondensed-Bold.ttf"
+// "assets\fonts\Open_Sans\static\OpenSans_SemiCondensed-BoldItalic.ttf"
+// "assets\fonts\Open_Sans\static\OpenSans_SemiCondensed-ExtraBold.ttf"
+// "assets\fonts\Open_Sans\static\OpenSans_SemiCondensed-ExtraBoldItalic.ttf"
+// "assets\fonts\Open_Sans\static\OpenSans_SemiCondensed-Italic.ttf"
+// "assets\fonts\Open_Sans\static\OpenSans_SemiCondensed-Light.ttf"
+// "assets\fonts\Open_Sans\static\OpenSans_SemiCondensed-LightItalic.ttf"
+// "assets\fonts\Open_Sans\static\OpenSans_SemiCondensed-Medium.ttf"
+// "assets\fonts\Open_Sans\static\OpenSans_SemiCondensed-MediumItalic.ttf"
+// "assets\fonts\Open_Sans\static\OpenSans_SemiCondensed-Regular.ttf"
+// "assets\fonts\Open_Sans\static\OpenSans_SemiCondensed-SemiBold.ttf"
+// "assets\fonts\Open_Sans\static\OpenSans_SemiCondensed-SemiBoldItalic.ttf"
+// "assets\fonts\Open_Sans\static\OpenSans-Bold.ttf"
+// "assets\fonts\Open_Sans\static\OpenSans-BoldItalic.ttf"
+// "assets\fonts\Open_Sans\static\OpenSans-ExtraBold.ttf"
+// "assets\fonts\Open_Sans\static\OpenSans-ExtraBoldItalic.ttf"
+// "assets\fonts\Open_Sans\static\OpenSans-Italic.ttf"
+// "assets\fonts\Open_Sans\static\OpenSans-Light.ttf"
+// "assets\fonts\Open_Sans\static\OpenSans-LightItalic.ttf"
+// "assets\fonts\Open_Sans\static\OpenSans-Medium.ttf"
+// "assets\fonts\Open_Sans\static\OpenSans-MediumItalic.ttf"
+// "assets\fonts\Open_Sans\static\OpenSans-Regular.ttf"
+// "assets\fonts\Open_Sans\static\OpenSans-SemiBold.ttf"
+// "assets\fonts\Open_Sans\static\OpenSans-SemiBoldItalic.ttf"
+// "assets\fonts\Open_Sans\static\OpenSans_Condensed-Bold.ttf"
+// "assets\fonts\Open_Sans\static\OpenSans_Condensed-BoldItalic.ttf"
+// "assets\fonts\Open_Sans\static\OpenSans_Condensed-ExtraBold.ttf"
+// "assets\fonts\Open_Sans\static\OpenSans_Condensed-ExtraBoldItalic.ttf"
+
+
+
+// "assets\fonts\Poppins\Poppins-Bold.ttf"
+// "assets\fonts\Poppins\Poppins-BoldItalic.ttf"
+// "assets\fonts\Poppins\Poppins-ExtraBold.ttf"
+// "assets\fonts\Poppins\Poppins-ExtraBoldItalic.ttf"
+// "assets\fonts\Poppins\Poppins-ExtraLight.ttf"
+// "assets\fonts\Poppins\Poppins-ExtraLightItalic.ttf"
+// "assets\fonts\Poppins\Poppins-Italic.ttf"
+// "assets\fonts\Poppins\Poppins-Light.ttf"
+// "assets\fonts\Poppins\Poppins-LightItalic.ttf"
+// "assets\fonts\Poppins\Poppins-Medium.ttf"
+// "assets\fonts\Poppins\Poppins-MediumItalic.ttf"
+// "assets\fonts\Poppins\Poppins-Regular.ttf"
+// "assets\fonts\Poppins\Poppins-SemiBold.ttf"
+// "assets\fonts\Poppins\Poppins-SemiBoldItalic.ttf"
+// "assets\fonts\Poppins\Poppins-Thin.ttf"
+// "assets\fonts\Poppins\Poppins-ThinItalic.ttf"
+// "assets\fonts\Poppins\Poppins-Black.ttf"
+// "assets\fonts\Poppins\Poppins-BlackItalic.ttf"
+
+
+
+// "assets\fonts\Raleway\static\Raleway-BoldItalic.ttf"
+// "assets\fonts\Raleway\static\Raleway-ExtraBold.ttf"
+// "assets\fonts\Raleway\static\Raleway-ExtraBoldItalic.ttf"
+// "assets\fonts\Raleway\static\Raleway-ExtraLight.ttf"
+// "assets\fonts\Raleway\static\Raleway-ExtraLightItalic.ttf"
+// "assets\fonts\Raleway\static\Raleway-Italic.ttf"
+// "assets\fonts\Raleway\static\Raleway-Light.ttf"
+// "assets\fonts\Raleway\static\Raleway-LightItalic.ttf"
+// "assets\fonts\Raleway\static\Raleway-Medium.ttf"
+// "assets\fonts\Raleway\static\Raleway-MediumItalic.ttf"
+// "assets\fonts\Raleway\static\Raleway-Regular.ttf"
+// "assets\fonts\Raleway\static\Raleway-SemiBold.ttf"
+// "assets\fonts\Raleway\static\Raleway-SemiBoldItalic.ttf"
+// "assets\fonts\Raleway\static\Raleway-Thin.ttf"
+// "assets\fonts\Raleway\static\Raleway-ThinItalic.ttf"
+// "assets\fonts\Raleway\static\Raleway-Black.ttf"
+// "assets\fonts\Raleway\static\Raleway-BlackItalic.ttf"
+// "assets\fonts\Raleway\static\Raleway-Bold.ttf"
+
+
+
+// "assets\fonts\Roboto_Mono\static\RobotoMono-Bold.ttf"
+// "assets\fonts\Roboto_Mono\static\RobotoMono-BoldItalic.ttf"
+// "assets\fonts\Roboto_Mono\static\RobotoMono-ExtraLight.ttf"
+// "assets\fonts\Roboto_Mono\static\RobotoMono-ExtraLightItalic.ttf"
+// "assets\fonts\Roboto_Mono\static\RobotoMono-Italic.ttf"
+// "assets\fonts\Roboto_Mono\static\RobotoMono-Light.ttf"
+// "assets\fonts\Roboto_Mono\static\RobotoMono-LightItalic.ttf"
+// "assets\fonts\Roboto_Mono\static\RobotoMono-Medium.ttf"
+// "assets\fonts\Roboto_Mono\static\RobotoMono-MediumItalic.ttf"
+// "assets\fonts\Roboto_Mono\static\RobotoMono-Regular.ttf"
+// "assets\fonts\Roboto_Mono\static\RobotoMono-SemiBold.ttf"
+// "assets\fonts\Roboto_Mono\static\RobotoMono-SemiBoldItalic.ttf"
+// "assets\fonts\Roboto_Mono\static\RobotoMono-Thin.ttf"
+// "assets\fonts\Roboto_Mono\static\RobotoMono-ThinItalic.ttf"
+
+
+
+// "assets\fonts\Winky_Sans\static\WinkySans-Light.ttf"
+// "assets\fonts\Winky_Sans\static\WinkySans-LightItalic.ttf"
+// "assets\fonts\Winky_Sans\static\WinkySans-Medium.ttf"
+// "assets\fonts\Winky_Sans\static\WinkySans-MediumItalic.ttf"
+// "assets\fonts\Winky_Sans\static\WinkySans-Regular.ttf"
+// "assets\fonts\Winky_Sans\static\WinkySans-SemiBold.ttf"
+// "assets\fonts\Winky_Sans\static\WinkySans-SemiBoldItalic.ttf"
+// "assets\fonts\Winky_Sans\static\WinkySans-Black.ttf"
+// "assets\fonts\Winky_Sans\static\WinkySans-BlackItalic.ttf"
+// "assets\fonts\Winky_Sans\static\WinkySans-Bold.ttf"
+// "assets\fonts\Winky_Sans\static\WinkySans-BoldItalic.ttf"
+// "assets\fonts\Winky_Sans\static\WinkySans-ExtraBold.ttf"
+// "assets\fonts\Winky_Sans\static\WinkySans-ExtraBoldItalic.ttf"
+// "assets\fonts\Winky_Sans\static\WinkySans-Italic.ttf"
+
+
+
